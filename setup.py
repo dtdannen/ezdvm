f53f49ee3ffef34dc98d3b1783565e788ebb5df1
Dustin Dannenhauer <dustin.td@gmail.com>
2024-08-26 11:22:04 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezdvm",
-    version="0.1.0",
+    version="0.1.1",
     author="Dustin Dannenhauer",
     author_email="dustin@dvmdash.live",
     description="A simple python Data Vending Machine (DVM) implementation on nostr",
